db689ae72775ad1148d736f6db77ad3a6a584064
Bayronix <151779642+Bayronix@users.noreply.github.com>
2024-08-14 13:27:37 +0200

    .

--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,47 @@
-// import { callback } from "./basic/1";
-// import { person } from "./basic/2";
-// import { showMessage, calc, customError } from "./basic/4";
-// import { isWeekend, DayOfWeek } from "./basic/5";
-// import { mango, poly } from "./basic/6";
-// import { page1, page2 } from "./basic/7";
-// import { variable, status } from "./basic/3";
-// //
-// import { result } from "./generics/2";
-// import { merged } from "./generics/3";
-// import { createOrUpdateUserValue } from "./generics/4";
-// import { RoleDescription, UserRole } from "./generics/5";
-// import { Params } from "./generics/6";
-// // basic
-// console.log("///////// basic ////////////");
-// console.log(`1.ts: ${callback(50)}`);
-// console.log(`2.ts: ${person}`);
+import { callback } from "./basic/1";
+import { person } from "./basic/2";
+import { showMessage, calc, customError } from "./basic/4";
+import { isWeekend, DayOfWeek } from "./basic/5";
+import { mango, poly } from "./basic/6";
+import { page1, page2 } from "./basic/7";
+import { variable, status } from "./basic/3";
+//
+import { result } from "./generics/2";
+import { merged } from "./generics/3";
+import { createOrUpdateUserValue } from "./generics/4";
+import { RoleDescription, UserRole } from "./generics/5";
 
-// console.log(`3.ts: variable = ${variable}, status = ${status} `);
-// console.log(`4.ts: ${showMessage("Error")}, ${calc(202, 202)}, ${customError}`);
+// basic
+console.log("///////// basic ////////////");
+console.log(`1.ts: ${callback(50)}`);
+console.log(`2.ts: ${person}`);
 
-// const test: DayOfWeek[] = [
-//   DayOfWeek.Monday,
-//   DayOfWeek.Saturday,
-//   DayOfWeek.Sunday,
-// ];
+console.log(`3.ts: variable = ${variable}, status = ${status} `);
+console.log(`4.ts: ${showMessage("Error")}, ${calc(202, 202)}, ${customError}`);
 
-// console.log(
-//   `5.ts: Monday - ${isWeekend(DayOfWeek.Monday)}, Saturday - ${isWeekend(
-//     DayOfWeek.Saturday
-//   )}, Sunday - ${isWeekend(DayOfWeek.Sunday)}`
-// );
+const test: DayOfWeek[] = [
+  DayOfWeek.Monday,
+  DayOfWeek.Saturday,
+  DayOfWeek.Sunday,
+];
 
-// console.log(
-//   `6.ts: mango: ${JSON.stringify(mango)}, poly: ${JSON.stringify(poly)}`
-// );
+console.log(
+  `5.ts: Monday - ${isWeekend(DayOfWeek.Monday)}, Saturday - ${isWeekend(
+    DayOfWeek.Saturday
+  )}, Sunday - ${isWeekend(DayOfWeek.Sunday)}`
+);
 
-// console.log(
-//   `7.ts: page1: ${JSON.stringify(page1)}, page2: ${JSON.stringify(page2)}`
-// );
-// // generics
-// console.log("////////// generics ///////////");
+console.log(
+  `6.ts: mango: ${JSON.stringify(mango)}, poly: ${JSON.stringify(poly)}`
+);
 
-// console.log(`2.ts: ${JSON.stringify(result)}`);
-// console.log(`3.ts: ${JSON.stringify(merged)} `);
-// console.log(`4.ts: ${JSON.stringify(createOrUpdateUserValue)}`);
-// console.log(`5.ts: ${RoleDescription[UserRole.admin]} `);
+console.log(
+  `7.ts: page1: ${JSON.stringify(page1)}, page2: ${JSON.stringify(page2)}`
+);
+// generics
+console.log("////////// generics ///////////");
+
+console.log(`2.ts: ${JSON.stringify(result)}`);
+console.log(`3.ts: ${JSON.stringify(merged)} `);
+console.log(`4.ts: ${JSON.stringify(createOrUpdateUserValue)}`);
+console.log(`5.ts: ${RoleDescription[UserRole.admin]} `);
